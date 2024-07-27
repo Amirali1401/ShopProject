bdfd4e7f197c198f91047c9ed676fde61c0585eb
Amirali1401 <104853924+Amirali1401@users.noreply.github.com>
2024-07-27 20:32:11 +0330

    Update settings.py

--- ShopProject/settings.py
+++ ShopProject/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-$256nd(ukpc&15+sr7k&td)o*=z)7lnt^$^qy$)!)0+4!yjmn('
+SECRET_KEY = 'was hidden by author'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
